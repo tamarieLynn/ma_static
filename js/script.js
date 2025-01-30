eb2c5049b717a2d268861682b4126955058dd2df
Tcarri <133730561+Tcarri@users.noreply.github.com>
2025-01-30 11:23:07 -0800

    Updating for mobile friendly photos

--- js/script.js
+++ js/script.js
@@ -17,4 +17,30 @@ next.addEventListener('click', () => {
     showImage(index);
 });
 
+// Touch support for mobile swiping
+let touchStartX = 0;
+let touchEndX = 0;
+
+document.querySelector('.carousel').addEventListener('touchstart', (e) => {
+    touchStartX = e.touches[0].clientX;
+});
+
+document.querySelector('.carousel').addEventListener('touchend', (e) => {
+    touchEndX = e.changedTouches[0].clientX;
+    handleSwipe();
+});
+
+function handleSwipe() {
+    const swipeThreshold = 50; // Minimum swipe distance to trigger action
+    if (touchStartX - touchEndX > swipeThreshold) {
+        // Swipe left (Next)
+        index = (index < images.length - 1) ? index + 1 : 0;
+    } else if (touchEndX - touchStartX > swipeThreshold) {
+        // Swipe right (Previous)
+        index = (index > 0) ? index - 1 : images.length - 1;
+    }
+    showImage(index);
+}
+
+// Initialize first image
 showImage(index);
