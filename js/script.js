3f8a57813423ef814cc1f36e59267e70087763cf
Tcarri <133730561+Tcarri@users.noreply.github.com>
2025-02-05 15:28:09 -0800

    trying to fix back to top button

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 let index = 0;
-const images = document.querySelectorAll('.page');
+const images = document.querySelectorAll('.frame-container');
 const prev = document.getElementById('prev');
 const next = document.getElementById('next');
 
